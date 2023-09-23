d8e0f46ba9556c30c0b5eb19f173f122709e7d8a
hisato <h.takeuchi.engineer@gmail.com>
2023-09-23 21:13:33 +0900

    feat:レコード一覧レイアウト削除

--- components/RecordItem.tsx
+++ components/RecordItem.tsx
@@ -14,35 +14,5 @@ export const RecordItem: FC<Omit<Record, 'created_at' | 'user_id'>> = ({
 }) => {
   const update = useStore((state) => state.updateEditedRecord)
   const { deleteRecordMutation } = useMutateRecord()
-  return (
-    <li className="my-3 justify-between text-lg font-extrabold">
-      <div>
-        <span>{plant_id}</span>
-        <span>水やり：{is_water ? '◯' : '-'}</span>
-        <span>肥料：{is_fertilizer ? '◯' : '-'}</span>
-        <span>殺虫・殺菌：{is_chemical ? '◯' : '-'}</span>
-      </div>
-      <div className="flex">
-        <PencilAltIcon
-          className="mx-1 h-5 w-5 cursor-pointer text-green-500"
-          onClick={() => {
-            update({
-              id,
-              record_date,
-              plant_id,
-              is_water,
-              is_fertilizer,
-              is_chemical,
-            })
-          }}
-        />
-        <TrashIcon
-          className="h-5 w-5 cursor-pointer text-green-500"
-          onClick={() => {
-            deleteRecordMutation.mutate(id)
-          }}
-        />
-      </div>
-    </li>
-  )
+  return <li className="my-3 justify-between text-lg font-extrabold"></li>
 }
