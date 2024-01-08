a06cf5d0aee85f0148771d0f4cd3d71c7a58d404
hisato <h.takeuchi.engineer@gmail.com>
2024-01-08 18:21:48 +0900

    ✨ feat:オフライン時のカスタムフック作成

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,10 @@
 import { FC, ReactNode, useEffect, useState } from 'react'
 import Head from 'next/head'
-import { BadgeCheckIcon } from '@heroicons/react/solid'
-import { Navbar, NavbarBrand, useUser } from '@nextui-org/react'
 import { AppHeader } from './AppHeader'
 import { supabase } from '../utils/supabase'
 import { AppFooter } from './AppFooter'
-import { User } from '@supabase/gotrue-js'
+import { Alert } from '@mui/material'
+import { useOnlineStatus } from '../hooks/useOnlineStatus'
 
 type Title = {
   title: string
@@ -13,6 +12,7 @@ type Title = {
 }
 export const Layout: FC<Title> = ({ children, title = '草ログ' }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const isOnlineStatus = useOnlineStatus()
   const checkLoggedInUser = async () => {
     // ログインのセッションを取得する処理
     const {
@@ -26,19 +26,30 @@ export const Layout: FC<Title> = ({ children, title = '草ログ' }) => {
   }, [])
 
   return (
-    <div className="flex min-h-screen flex-col items-center text-gray-800">
-      <Head>
-        <title>{title}</title>
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1.0,maximum-scale=1.0"
-        />
-      </Head>
-      {isLoggedIn && <AppHeader />}
-      <main className="flex w-screen flex-1 flex-col items-center justify-center">
-        {children}
-      </main>
-      {isLoggedIn && <AppFooter />}
-    </div>
+    <>
+      {!isOnlineStatus && (
+        <Alert
+          variant="filled"
+          severity="error"
+          className="sticky inset-y-0 z-[9999]"
+        >
+          ネットワークに接続していません。
+        </Alert>
+      )}
+      <div className="flex min-h-screen flex-col items-center text-gray-800">
+        <Head>
+          <title>{title}</title>
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1.0,maximum-scale=1.0"
+          />
+        </Head>
+        {isLoggedIn && <AppHeader />}
+        <main className="flex w-screen flex-1 flex-col items-center justify-center">
+          {children}
+        </main>
+        {isLoggedIn && <AppFooter />}
+      </div>
+    </>
   )
 }
